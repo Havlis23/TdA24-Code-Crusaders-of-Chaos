0c4b36fbd97caf4ad75daba8a4753eb18d0f38f3
David Havel <davidek.havel@gmail.com>
2023-11-26 19:45:52 +0100

    add lecturer.html, update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-import os
+# app.py
 
-from flask import Flask
+import os
+from flask import Flask, render_template
 from . import db
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'tourdeflask.sqlite'),
 )
 
-# ensure the instance folder exists
+# Ensure the instance folder exists
 try:
     os.makedirs(app.instance_path)
 except OSError:
@@ -17,11 +18,36 @@ except OSError:
 
 db.init_app(app)
 
+teacher_data = {
+    "UUID": "67fda282-2bca-41ef-9caf-039cc5c8dd69",
+    "title_before": "Mgr.",
+    "first_name": "Petra",
+    "middle_name": "Swil",
+    "last_name": "Plachá",
+    "title_after": "MBA",
+    "picture_url": "https://tourdeapp.cz/storage/images/2023_02_25/412ff296a291f021bbb6de10e8d0b94863fa89308843b/big.png.webp",
+    "location": "Brno",
+    "claim": "Aktivní studentka / Předsedkyně spolku / Projektová manažerka",
+    "bio": "<p>Baví mě organizovat věci. Ať už to bylo vyvíjení mobilních aplikací ve Futured, pořádání konferencí, spolupráce na soutěžích Prezentiáda, pIšQworky, <b>Tour de App</b> a Středoškolák roku, nebo třeba dobrovolnictví, vždycky jsem skončila u projektového managementu, rozvíjení soft-skills a vzdělávání. U studentských projektů a akcí jsem si vyzkoušela snad všechno od marketingu po logistiku a moc ráda to předám dál. Momentálně studuji Pdf MUNI a FF MUNI v Brně.</p>",
+    "tags": [
+        {"uuid": "6d348a49-d16f-4524-86ac-132dd829b429", "name": "Dobrovolnictví"},
+        {"uuid": "8e0568c3-e235-42aa-9eaa-713a2545ff5b", "name": "Studentské spolky"},
+        # ... (other tags)
+    ],
+    "price_per_hour": 1200,
+    "contact": {
+        "telephone_numbers": ["+420 722 482 974"],
+        "emails": ["placha@scg.cz", "predseda@scg.cz"]
+    }
+}
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return "Hello TdA"
 
+@app.route('/lecturer')
+def teacher_profile():
+    return render_template('lecturer.html', teacher_name=f"{teacher_data['first_name']} {teacher_data['last_name']}", teacher_data=teacher_data)
 
 if __name__ == '__main__':
     app.run()
