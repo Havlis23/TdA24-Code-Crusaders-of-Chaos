c3380c9f55f5b190872ab7047248abbca7685b26
David Havel <davidek.havel@gmail.com>
2023-12-03 22:19:51 +0100

    api does work with limitations, cant fetch contact info

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LecturerList from './LecturerList';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Your React App</h1>
+            <LecturerList/>
+        </div>
+    );
+};
+
+export default App;
